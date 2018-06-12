eedbefd1d79b32f23bd14702efe553503dea656d
Derek Minney <derekminney@Dereks-MacBook-Air.local>
2018-06-12 10:33:32 -0600

    dereks-branch

---
+++ server.js
@@ -0,0 +1,50 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("./app/config/passport.js");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./app/models");
+
+//passport set-up
+app.use(session({secret: 'mySecretKey', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+//set handlebars 
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./app/routes/html_routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
